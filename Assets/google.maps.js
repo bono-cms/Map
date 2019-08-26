c3bef8c970dd48a77d19e8a8c62b069466f2a82c
dave-yang <daworld.ny@gmail.com>
2019-08-27 00:54:21 +0500

    Render multiple maps without conflicts

--- Assets/google.maps.js
+++ Assets/google.maps.js
@@ -1,42 +1,54 @@
+(function(document, JSON, google){
+    // Renders a map on given HTML element
+    var renderMap = function(element){
+        // Parse map configuration of the current element
+        var config = JSON.parse(element.value);
 
-(function(config){
-    var element = document.getElementById(config.id);
-    var map = new google.maps.Map(element, {
-        scrollwheel: false, 
-        zoom: parseInt(config.zoom), 
-        center: {
-            lat: parseFloat(config.lat),
-            lng: parseFloat(config.lng)
-        }, 
-        styles: config.style ? JSON.parse(config.style) : []
-    });
+        var map = new google.maps.Map(document.getElementById(config.id), {
+            scrollwheel: false, 
+            zoom: parseInt(config.zoom), 
+            center: {
+                lat: parseFloat(config.lat),
+                lng: parseFloat(config.lng)
+            }, 
+            styles: config.style ? JSON.parse(config.style) : []
+        });
 
-    // Draw markers if available
-    if (config.markers.length) {
-        for (i = 0; i < config.markers.length; i++) {
-            // Current marker
-            var current = config.markers[i];
+        // Draw markers if available
+        if (config.markers.length) {
+            for (i = 0; i < config.markers.length; i++) {
+                // Current marker
+                var current = config.markers[i];
 
-            var marker = new google.maps.Marker({
-                draggable: current.draggable == 1,
-                position: {
-                    lat: parseFloat(current.lat),
-                    lng: parseFloat(current.lng)
-                }, 
-                map: map
-            });
-
-            // If description provided, then attach InfoWindow
-            if (current.description) {
-                var infowindow = new google.maps.InfoWindow({
-                    content: (current.description)
+                var marker = new google.maps.Marker({
+                    draggable: current.draggable == 1,
+                    position: {
+                        lat: parseFloat(current.lat),
+                        lng: parseFloat(current.lng)
+                    }, 
+                    map: map
                 });
 
-                marker.addListener('click', function() {
-                    infowindow.open(map, marker);
-                });
+                // If description provided, then attach InfoWindow
+                if (current.description) {
+                    var infowindow = new google.maps.InfoWindow({
+                        content: (current.description)
+                    });
+
+                    marker.addListener('click', function() {
+                        infowindow.open(map, marker);
+                    });
+                }
             }
         }
+    };
+
+    var maps = document.getElementsByClassName('map-configuration-input');
+
+    // Render maps
+    for (var index in maps) {
+        var map = maps[index];
+        renderMap(map);
     }
 
-})(window.mapConfig);
+})(document, JSON, google);
